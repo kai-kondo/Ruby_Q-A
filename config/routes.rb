bd885210836628dc80d533b9dd06393d82bbfc34
kai-kondo <rad.kai0404@gmail.com>
2023-11-15 14:14:11 +0000

    新規質問登録機能、質問の一覧表示、削除、編集、コメント追加、削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :questions
+  
+  root "questions#index"
+  resources :questions do
+  resources :answers
+  end
 end
