dc477954b680950ac49d09a9fce4585b7505b2b6
kai-kondo <rad.kai0404@gmail.com>
2023-11-20 12:54:14 +0000

    new画面とedit画面に年齢フォームの追加・一覧画面に年齢を表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_15_131354) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_19_075422) do
   create_table "answers", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_131354) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "old"
   end
 
   add_foreign_key "answers", "questions"
