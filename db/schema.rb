f968e7a51f1ac59e84c8fe9242e1e8e071e14eac
kai-kondo <rad.kai0404@gmail.com>
2023-12-07 06:47:01 +0900

    docker環境構築のためマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_11_19_075422) do
-  create_table "answers", force: :cascade do |t|
+  create_table "answers", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.text "content"
-    t.integer "question_id", null: false
+    t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table "questions", force: :cascade do |t|
+  create_table "questions", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.text "content"
