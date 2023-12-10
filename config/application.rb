13781b896d5d0738c4c08400a176cfc0fa06222e
kai-kondo <rad.kai0404@gmail.com>
2023-12-10 14:34:54 +0900

    erbからslim形式に変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Qanda
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    cifug.generators.template_engine = :slim
 
     # Configuration for the application, engines, and railties goes here.
     #
